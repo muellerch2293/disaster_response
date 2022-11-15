7a3d166b5435ab47c133ba755f6374c7bb72ecb0
Christian Müller <christian.mueller@spar-ics.com>
2022-11-15 17:23:31 +0100

    refactored etl pipeline

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -4,7 +4,20 @@ import sqlite3
 from sqlalchemy import create_engine
 
 
-def main():
+
+import sys
+
+
+def load_data(messages_filepath, categories_filepath):
+    """
+    Correlates and loads raw data into a pandas dataframe 
+    messages_filepath : str
+        path pointing to the file containing the messages
+    categories_filepath : str
+        path pointing to the file containing the categorization of the messages
+    Return:
+    pandas dataframe containing the messages with their respective categories   
+    """
     # load messages dataset
     messages = pd.read_csv('data/disaster_messages.csv')
 
@@ -13,7 +26,17 @@ def main():
 
     # merge datasets
     df = messages.merge(categories,on='id')
-    
+    return df
+
+
+def clean_data(df):
+    """
+    Cleans categories of messages st. every category has its own column that could be either 0 or 1
+    df : pandas dataframe
+        dataframe containing messages with their categories (in a single column, seperated by semicolons)
+    Return:
+    pandas dataframe with messages and their cleaned up classification  
+    """
     # create a dataframe of the 36 individual category columns
     categories = df.categories.str.split(';',expand=True)
     
@@ -38,17 +61,51 @@ def main():
     # concatenate the original dataframe with the new `categories` dataframe
     df = pd.concat([df,categories],axis=1,join = 'inner')
     
-    # concatenate the original dataframe with the new `categories` dataframe
-    df = pd.concat([df,categories],axis=1,join = 'inner')
-    
     # drop duplicates
     df.drop_duplicates(inplace = True)
-    engine = create_engine('sqlite:///data/disaster_response.db')
+    return df
+
+def save_data(df, database_filename):
+    """
+    Saves dataframe to a database with the given name
+    df : pandas dataframe
+        dataframe that should be persisted in the database
+    database_filename: str
+        location of the database file in which the dataframe should be stored
+    """
+    engine = create_engine(('sqlite:///%s' % database_filename))
     #drop table if exists 
     sql = 'DROP TABLE IF EXISTS disaster_messages;'
     result = engine.execute(sql)
     df.to_sql('disaster_messages', engine, index=False)
     print("Cleaned data and saved into disaster_messages table!")
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
     
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
 if __name__ == '__main__':
     main()
